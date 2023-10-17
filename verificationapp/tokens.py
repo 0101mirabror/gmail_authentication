3ef1db23655c3665304a69db1ac1c76fe4e41d2b
Mirabror <fayzullayevmirabror2@gmail.com>
2023-10-17 20:19:55 +0500

    all things are working

--- verificationapp/tokens.py
+++ verificationapp/tokens.py
@@ -1,13 +1,23 @@
-# appname/tokens.py
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-import six
+from django.contrib.sites.shortcuts import get_current_site
 
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return (
-            six.text_type(user.pk) + six.text_type(timestamp) +
-            six.text_type(user.email_confirmed)
-        )
+def send_mail_after_registration(email, token, username, request):
+    subject = "Your account needs to be verified"
+    # message = f'''Hi paste your link to verify your account <p><a href="https://yourwebsite.com/verify?token=xyz123">Verify Email</a></p> http://127.0.0.1:8000/verify/{token}
+   
+    # '''
+    message = render_to_string('gmail_verification_link.html', {
+        'user': username,
+        'domain': get_current_site(request).domain, 
+        'token': token,
+        "protocol": 'https' if request.is_secure() else 'http'
+    })
+    print(message)
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message , email_from ,recipient_list)
 
-account_activation_token = AccountActivationTokenGenerator()
+    print("all is good")
--- verificationapp/urls.py
+++ verificationapp/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('login/' , login_attempt , name='login_attempt'),
     path('token' , token_send , name='token_send'),
     path('success' , success, name ='success'),
-    path('verify/<auth_token>' , verify ,name = "verify"),
+    path('verify/<token>' , verify ,name = "verify"),
     path('error' , error_page , name = "error")
 ]
